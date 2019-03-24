dba936495c1f4343ff233441d1ad3ed15fcbbccf
Justin Wu <justinhwu95@gmail.com>
2019-03-24 19:36:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_japanese_emoticon(file_path, english)
     return lib["get_emoticon"][english]
   else
     return "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
