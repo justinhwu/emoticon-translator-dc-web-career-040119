dc53b5ac1b8b1e7056ae6c287a7448e408b5508d
Justin Wu <justinhwu95@gmail.com>
2019-03-24 19:25:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   lib = YAML.load_file(file_path)
   hash = {"get_meaning": {}, "get_emoticon": {}}
   lib.each do |a,b|
-    hash["get_meaning"][b]
+    hash["get_meaning"][b] = 
 end
 
 def get_japanese_emoticon
