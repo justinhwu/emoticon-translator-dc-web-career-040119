d95805c9c3e95fc009fe75f435d1372b847f238b
Justin Wu <justinhwu95@gmail.com>
2019-03-24 19:36:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,7 @@ def get_japanese_emoticon(file_path, english)
   lib = load_library(file_path)
   if lib["get_emoticon"].has_value?(english)
     lib["get_emoticon"][english]
+  else
 end
 
 def get_english_meaning
