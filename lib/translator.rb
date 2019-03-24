90dbf6c39b2cc8f3691e4860be8e4d0ab5c796f2
Justin Wu <justinhwu95@gmail.com>
2019-03-24 19:38:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_path, english)
   # code goes here
   lib = load_library(file_path)
-  if lib.has_value?(english)
+  if lib.include?(english)
     return lib["get_emoticon"][english]
   else
     return "Sorry, that emoticon was not found"
